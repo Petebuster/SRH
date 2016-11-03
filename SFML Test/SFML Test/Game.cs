4437b188dff231f238756f01cc547d353b031485
Discepless <discepless@yandex.ru>
2016-11-03 10:22:11 +0100

    Add files via upload

---
+++ SFML Test/SFML Test/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SFML_Test
+{
+    abstract class Game
+    {
+        protected RenderWindow Window;
+        protected Color ClearColor;
+
+        protected Game(uint width, uint height, string title, Color ClearColor)
+        {
+            this.Window = new RenderWindow(new VideoMode(width, height),title, Styles.Close);
+            this.ClearColor = ClearColor;
+
+            Window.Closed += OnClosed;
+        }
+
+       
+
+        public void Run()
+        {
+            LoadContent();
+            Initialize();
+
+            while (Window.IsOpen)
+            {
+                Window.DispatchEvents();
+                Tick();
+
+                Window.Clear(ClearColor);
+                Render();
+                Window.Display();
+            }
+
+        }
+
+        protected abstract void LoadContent();
+        protected abstract void Initialize();
+
+        protected abstract void Tick();
+        protected abstract void Render();
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            Window.Close();
+        }
+    }
+}
---
+++ SFML Test/SFML Test/MainGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SFML_Test
+{
+    class MainGame : Game
+    {
+        public MainGame()
+            : base(800,600,"Test", Color.Yellow)
+        {
+            
+        }
+
+        protected override void LoadContent()
+        {
+        }
+
+        protected override void Initialize()
+        {
+        }
+
+        protected override void Tick()
+        {
+        }
+
+        protected override void Render()
+        {           
+        }
+    }
+}
